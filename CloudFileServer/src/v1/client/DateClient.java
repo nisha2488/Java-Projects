f149b578d9656a639641c221fc2c0fc06d9cb731
Nisha <nisha2488@gmail.com>
2018-08-11 17:17:10 -0700

    Added new line

--- CloudFileServer/src/v1/client/DateClient.java
+++ CloudFileServer/src/v1/client/DateClient.java
@@ -7,7 +7,7 @@ public class DateClient {
 	public static void main(String args[]) throws Exception
     {
         Socket soc=new Socket("192.168.56.110", 5217);        
-        BufferedReader in=new BufferedReader(new InputStreamReader(soc.getInputStream()));
+        BufferedReader in= new BufferedReader(new InputStreamReader(soc.getInputStream()));
         StringBuilder sb = new StringBuilder();
         while(true) {
         	String currentLine = in.readLine();
