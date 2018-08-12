272104c7778d685cd0482bb8d2d4f8283a46257e
Nisha <nisha2488@gmail.com>
2018-08-11 21:03:30 -0700

    Adding v2

---
+++ CloudFileServer/src/v2/client/DateClient.java
@@ -0,0 +1,23 @@
+package v2.client;
+
+import java.io.*;
+import java.net.*;
+
+public class DateClient {
+	public static void main(String args[]) throws Exception
+    {
+        Socket soc=new Socket("192.168.56.110", 5217);        
+        DataOutputStream out = new DataOutputStream(soc.getOutputStream());
+        out.writeByte(Integer.parseInt(args[0]));
+        BufferedReader in= new BufferedReader(new InputStreamReader(soc.getInputStream()));
+        StringBuilder sb = new StringBuilder();
+        while(true) {
+        	String currentLine = in.readLine();
+        	if(currentLine == null || currentLine.isEmpty()) {
+        		break;
+        	}
+        	sb.append(currentLine);
+        }
+        System.out.println(sb);
+    }    
+}
---
+++ CloudFileServer/src/v2/server/DateServer.java
@@ -0,0 +1,40 @@
+package v2.server;
+
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+public class DateServer {
+	public static void main(String args[]) throws Exception {
+        ServerSocket s=new ServerSocket(5217);
+        while(true) {
+            System.out.println("Waiting For Connection ...");
+            Socket soc = s.accept();
+            DataOutputStream out = new DataOutputStream(soc.getOutputStream());
+            DataOutputStream out2 = new DataOutputStream(soc.getOutputStream());
+            BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+            if(in.read() == 1) {
+            	out.writeBytes("Server Date: " + (new Date()).toString());
+            }
+            else {
+            	out.writeBytes("Server Address: " + InetAddress.getLocalHost());
+            }
+//            out.writeBytes("Server Date: " + (new Date()).toString());
+            out2.writeBytes(" \n Data Sent!");
+            out.close();
+            out2.close();
+            soc.close();
+            s.close();
+        }
+
+    }
+}
+
+
+//--- 
+
+//class DateServerSlave implements Runnable {
+//  public void run() {
+//
+//  }
+//}
