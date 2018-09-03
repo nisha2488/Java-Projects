141f48a1acf22bfce6249b1df9093532f0b967a4
Nisha <nisha2488@gmail.com>
2018-09-03 16:29:53 -0700

    Removed multiple output lines from server

--- CloudFileServer/src/v2/server/DateServer.java
+++ CloudFileServer/src/v2/server/DateServer.java
@@ -42,14 +42,16 @@ class DateServerSlave implements Runnable {
 			DataOutputStream out = new DataOutputStream(soc.getOutputStream());
 	        BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
 	        if(in.read() == 1) {
+	        	System.out.println("Connection accepted for client: " + soc.getRemoteSocketAddress().toString());
 	        	out.writeBytes("Server Date: " + (new Date()).toString());
-	        	out.writeBytes("Current thread put to sleep at : " + (LocalDateTime.now()).toString());
+//	        	out.writeBytes("Current thread put to sleep at : " + (LocalDateTime.now()).toString());
 	        	//Sleep for 20 seconds to test multi-threading
 //	        	Thread.sleep(20000);
-	        	out.writeBytes("Current thread back to runnable at : " + (LocalDateTime.now()).toString());
+//	        	out.writeBytes("Current thread back to runnable at : " + (LocalDateTime.now()).toString());
+	        	System.out.println("Data sent back to the client: " + soc.getRemoteSocketAddress().toString());
 	        } else {
 	        	out.writeBytes("Server Address: " + InetAddress.getLocalHost());
-	        	out.writeBytes("Current thread executed at : " + (LocalDateTime.now()).toString());
+//	        	out.writeBytes("Current thread executed at : " + (LocalDateTime.now()).toString());
 	        }
 	        out.close();
 	        soc.close();
