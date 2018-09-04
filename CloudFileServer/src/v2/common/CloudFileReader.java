73bebf12be1b2d902c208ba891979d634dd91b80
Nisha <nisha2488@gmail.com>
2018-09-04 00:19:51 -0700

    updating

--- CloudFileServer/src/v2/common/CloudFileReader.java
+++ CloudFileServer/src/v2/common/CloudFileReader.java
@@ -2,21 +2,24 @@ package v2.common;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 
 public class CloudFileReader {
 	
 	public static final String FILE_DIR = "/home/nisha/Documents/Personal Git Repo/Java-Projects/CloudFileServer/src/files/";
+	public static final int CHUNK_SIZE = 1024;
 	
 	public static void main(String ...args) {
 		CloudFileReader clf = new CloudFileReader();
 		try {
-			String output = clf.readFileContents(FILE_DIR + "sampleHTML.html");
+			String output = clf.readFileChunk(FILE_DIR + "sampleHTML.html", 4);
+//			String output = clf.readFileContents(FILE_DIR + "sampleHTML.html");
 			System.out.println(output);
-		}
-		catch (Exception e) {
-			// TODO Auto-generated catch block
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
@@ -31,5 +34,18 @@ public class CloudFileReader {
 		}
 		br.close();
 		return fileContent.toString();
+	}
+	
+	public String readFileChunk(String fileName, int chunkNumber) throws Exception {
+		byte[] chunk = new byte[CHUNK_SIZE];
+		RandomAccessFile raf = new RandomAccessFile(fileName, "r");
+		if(raf.length()<=chunkNumber*CHUNK_SIZE) {
+			raf.close();
+			throw new Exception("Required chunk number "+chunkNumber+" does not exist in "+fileName);
+		}
+		raf.seek(chunkNumber*CHUNK_SIZE);
+		raf.read(chunk);
+		raf.close();
+		return new String(chunk);
 	}
 }
