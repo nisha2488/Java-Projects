fe020fa09904656f124079d173016c05eba61558
Nisha <nisha2488@gmail.com>
2018-09-15 22:03:34 -0700

    updating

--- CloudFileServer/src/v4/common/types/FileListServerMessage.java
+++ CloudFileServer/src/v4/common/types/FileListServerMessage.java
@@ -1,7 +1,12 @@
 package v4.common.types;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FileListServerMessage extends ServerMessage {
 	public List<FileManifest> fileManifestList;
+	
+	public FileListServerMessage() {
+		this.fileManifestList = new ArrayList<FileManifest>();
+	}
 }
